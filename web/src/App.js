ca321e170ee0e52074c5ef197b34c3047e4532ed
Jack Armstrong <jackarms@cs.washington.edu>
2018-02-07 22:31:46 -0800

    adding location input, turret css

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import LocationForm from './LocationForm';
 import './App.css';
+import './turret.css';
+
+const GOOGLE_MAPS_API_URI = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCBqE1PQyfBRxAUnLTk-ixdsmCk936YlW4";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">TactileMaptile</h1>
+        <header className="header">
+          <div className="container">
+            <h1>TactileMaptile</h1>
+          </div>
         </header>
-        <div>
-          <LocationForm />
-        </div>
+        <main role="main">
+          <div class="container">
+          <LocationForm 
+            googleMapURL={GOOGLE_MAPS_API_URI}
+            loadingElement={<div/>} />
+          </div>
+        </main>
       </div>
     );
   }
